d8cc2bf688e908152098dedff85102b1f7a18fb5
Nicolás Ballén <n.ballen1@uniandes.edu.co>
2025-10-06 18:17:12 -0500

    detalles

--- laboratorios/laboratorio3/Laboratorio3/ProtocoloTCP/publisher_tcp.c
+++ laboratorios/laboratorio3/Laboratorio3/ProtocoloTCP/publisher_tcp.c
@@ -1,39 +1,16 @@
-#include <netinet/in.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <unistd.h>
+#if  defined(_WIN32) || defined(_WIN64)
+    #include <winsock2.h> // For Windows sockets
+    #include <ws2tcpip.h> // For some Winsock functions like getaddrinfo
+    #pragma comment(lib, "Ws2_32.lib") // Link with Ws2_32.lib
+#else
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include <unistd.h> // For close()
+#endif
 #define PORT 8080
 int main(int argc, char const* argv[]) {
-    int sockfd;
-    struct sockaddr_in serv_addr;
-
-    // Crear socket
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Error al crear socket");
-        exit(EXIT_FAILURE);
-    }
-
-    // Configurar dirección del servidor
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(PORT);
-
-    // Enlazar socket
-    if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("Error al enlazar socket");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
-
-    // Escuchar conexiones
-    if (listen(sockfd, 3) < 0) {
-        perror("Error al escuchar");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Esperando conexiones en el puerto %d...\n", PORT);
+    
     return 0;
 }   
